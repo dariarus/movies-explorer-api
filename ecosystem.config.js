39634ebea0ae22486cf276238f583d11d0104b23
dariarus <rusanova_dv@mail.ru>
2022-11-11 01:22:19 +0300

    feature: добавить и сконфигурировать ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/diploma/level-1',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'my-films-api',
+    script: './dist/app.js',
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/dariarus/movies-explorer-api',
+      path: DEPLOY_PATH,
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
