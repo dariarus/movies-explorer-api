440931a8f2c9a31eae9936c2c39fe83af2f43d23
dariarus <rusanova_dv@mail.ru>
2023-01-22 16:53:54 +0300

    bugfix: Изменить подключение мидлвара для кук в app.ts: вернуть указание origin и credentials

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,10 @@ const runApp = () => {
   console.log(`Starting server with env: ${nodeEnv}`);
 
   // TODO: добавить ссылку на фронт после его публикации в файл .env.production
-  app.use(cors());
+  app.use(cors({
+    origin: frontendUrl,
+    credentials: true,
+  }));
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
