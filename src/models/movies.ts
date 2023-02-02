1ab3aee423dc28e9249b6892e827e57e2cd8b6ec
dariarus <rusanova_dv@mail.ru>
2023-02-02 15:00:03 +0300

    bugfix: Отредактировать валидацию в схеме для nameRU: много языков

--- src/models/movies.ts
+++ src/models/movies.ts
@@ -10,6 +10,7 @@ import {
   INCORRECT_RU_NAME,
   INCORRECT_EN_NAME,
 } from '../utils/request-messanges';
+import isAlphanumericLocales = validator.isAlphanumericLocales;
 
 const movieSchema = new mongoose.Schema({
   country: {
@@ -69,8 +70,7 @@ const movieSchema = new mongoose.Schema({
     type: String,
     require: true,
     validate: {
-      validator: (val: string) => validator.isAlphanumeric(val, 'ru-RU', { ignore: ' -:' })
-        || validator.isAlphanumeric(val, 'en-US', { ignore: ' -:' }),
+      validator: (val: string) => validator.isAlphanumeric(val, 'ru-RU' || 'en-US' || ('ru-RU' && 'en-US'), { ignore: ' -:' }),
       message: INCORRECT_RU_NAME,
     },
   },
